782f2fd278f080dd3351647bd9d8fcb615ceca08
MhmdRyhn <rayhan.cse12@gmail.com>
2020-07-31 02:17:29 +0600

    Update docker image tagging mechanism

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ set -e
 aws_profile="dev"
 account_id=$(aws sts --profile $aws_profile get-caller-identity | jq -r ".Account")
 
+tag=$(date +"%y%m%d-%H%M")
+
 # Build docker image
-docker build . -t "$account_id.dkr.ecr.eu-west-1.amazonaws.com/flask-container:latest"
+docker build . -t "$account_id.dkr.ecr.eu-west-1.amazonaws.com/flask-container:$tag"
 # Login to AWS ECR
 aws ecr get-login-password --profile $aws_profile --region eu-west-1 |
   docker login --username AWS --password-stdin "$account_id.dkr.ecr.eu-west-1.amazonaws.com"
 # Push the image to ECR
-docker push "$account_id.dkr.ecr.eu-west-1.amazonaws.com/flask-container:latest"
+docker push "$account_id.dkr.ecr.eu-west-1.amazonaws.com/flask-container:$tag"
